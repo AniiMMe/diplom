dabf84cc7e0c59faaecaf0d65f56a28c77fb6656
Ania <anna.anna.linnik@gmail.com>
2024-02-27 16:57:51 +0300

    add controller

---
+++ src/main/java/com/example/diplom/controller/StartController.java
@@ -0,0 +1,15 @@
+package com.example.diplom.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class StartController {
+    @GetMapping("/")
+    public String getStart()
+    {
+        return "/main";
+    }
+}
