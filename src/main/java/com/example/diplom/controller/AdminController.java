ff02806bd773a8d19c1227f5b0aac3ca11b51583
Mary <parmo1977@gmail.com>
2024-04-03 22:20:51 +0300

    add getMapping for panelAdmin

--- src/main/java/com/example/diplom/controller/AdminController.java
+++ src/main/java/com/example/diplom/controller/AdminController.java
@@ -1,20 +1,26 @@
 package com.example.diplom.controller;
 
-import com.example.diplom.entity.Client;
-import com.example.diplom.entity.Workers;
-import com.example.diplom.service.ClientService;
-import com.example.diplom.service.UserService;
+import com.example.diplom.entity.*;
+import com.example.diplom.service.*;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @AllArgsConstructor
 public class AdminController {
     private final UserService userService;
     private final ClientService clientService;
+    private final ProvidersService providersService;
+    private final AssortmentService assortmentService;
+    private final OrderService orderService;
+    private final ProductService productService;
+    private final SupplyService supplyService;
 
     @GetMapping("/admin")
     public String getStart() {
@@ -32,11 +38,13 @@ public class AdminController {
         return "/admin/admin-clients";
     }
     @GetMapping("/admin/providers")
-    public  String getProvidersPage(){
+    public  String getProvidersPage(Model model){
+        model.addAttribute("providers", providersService.getAllProvider());
         return "/admin/admin-providers";
     }
     @GetMapping("/admin/assortment")
-    public  String getAssortPage(){
+    public  String getAssortPage(Model model){
+        model.addAttribute("assortment", new Assortment());
         return "/admin/admin-ass";
     }
     @GetMapping("/admin/products")
@@ -45,6 +53,7 @@ public class AdminController {
     }
     @GetMapping("/admin/orders")
     public  String getOrdersPage(){
+
         return "/admin/admin-order";
     }
     @GetMapping("/admin/invent")
@@ -61,11 +70,13 @@ public class AdminController {
         return "/newClient";
     }
     @GetMapping("/admin/newProvider")
-    public  String getNewProviderPage(){
+    public  String getNewProviderPage(Model model){
+        model.addAttribute("provider", new Providers());
         return "/newProvider";
     }
     @GetMapping("/admin/newAssortment")
-    public  String getNewAssortmentrPage(){
+    public  String getNewAssortmentrPage(Model model){
+        model.addAttribute("assortment", new Assortment());
         return "/newAss";
     }
     @GetMapping("/admin/newUser")
@@ -75,15 +86,25 @@ public class AdminController {
         return "/newUser";
     }
     @GetMapping("/admin/newSupply")
-    public  String getNewSypplyPage(){
+    public  String getNewSypplyPage(Model model){
+        List<Product> products = new ArrayList<>();
+        List<String> assortment = new ArrayList<>();
+        model.addAttribute("provider", new Supply());
+        model.addAttribute("products", products);
+        model.addAttribute("assortment", assortment);
         return "/newSupply";
     }
     @GetMapping("/admin/newOrder")
-    public  String getNewOrderPage(){
+    public  String getNewOrderPage(Model model){
+        List<Product> products = new ArrayList<>();
+        List<String> assortment = new ArrayList<>();
+        model.addAttribute("orders", new Orders());
+        model.addAttribute("products", products);
+        model.addAttribute("assortment", assortment);
         return "/newOrder";
     }
     @GetMapping("/admin/newInvent")
-    public  String getNewInventPage(){
+    public  String getNewInventPage(Model model){
         return "/newInvent";
     }
 
