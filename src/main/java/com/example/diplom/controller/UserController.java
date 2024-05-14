e83b6631080360a1c41f5794d4a1b66ff25b2329
Ania <anna.anna.linnik@gmail.com>
2024-05-15 01:04:45 +0300

    клиенты на юзере

--- src/main/java/com/example/diplom/controller/UserController.java
+++ src/main/java/com/example/diplom/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.example.diplom.controller;
 
+import com.example.diplom.service.ClientService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class UserController {
+    private final ClientService clientService;
+
     @GetMapping("/user")
     public String getStart() {
         return "/user/userpanel";
     }
+    @GetMapping("/user/clients")
+    public  String getClientsPage(Model model){
+        model.addAttribute("clients", clientService.getAllClients());
+        return "/user/user-clients";
+}
 }
