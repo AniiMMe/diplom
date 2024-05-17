852574445ce0c0f0fbb4cf72dc9d1fd495cc875e
Ania <anna.anna.linnik@gmail.com>
2024-05-17 22:07:58 +0300

    моя боль

---
+++ src/main/java/com/example/diplom/dto/ClientDTO.java
@@ -0,0 +1,36 @@
+package com.example.diplom.dto;
+
+import com.example.diplom.entity.Orders;
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClientDTO {
+
+    private int clientId;
+    @NotEmpty
+    @NonNull
+    private String clientName;
+    @NotEmpty
+    @NonNull
+    private String clientAddress;
+    @NotEmpty
+    @NonNull
+    @Pattern(regexp = "^\\+(375|80)(29|33|25|44)\\d{7}$")
+    private String phone;
+    private String clientTel;
+    @NotEmpty
+    @NonNull
+    @Pattern(regexp = "^[A-z0-9_.-]{1,}@[A-z0-9_.-]{1,}\\.[A-z]{2,8}$")
+    private String clientEmail;
+
+    private List<Orders> orders;
+}
--- src/main/java/com/example/diplom/service/ClientService.java
+++ src/main/java/com/example/diplom/service/ClientService.java
@@ -1,12 +1,17 @@
 package com.example.diplom.service;
 
+import com.example.diplom.dto.ClientDTO;
 import com.example.diplom.entity.Client;
+
 import com.example.diplom.entity.Workers;
 import com.example.diplom.reposiroty.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +28,32 @@ public class ClientService {
         return null;
 
     }
+
+    public Map<String, String> checkErrorSwitch(ClientDTO returnClientDTO,  BindingResult result){
+        Map<String,String> descriptionError = new HashMap<>();
+        result.getFieldErrors().forEach(error ->{
+            switch (error.getField()){
+                case "fio":{
+                    descriptionError.put("fio", "Введите ФИО");
+                    break;
+                }
+                case "email":{
+                    descriptionError.put("email", "Введите Электронную почту");
+                    break;
+                }
+                case "address":{
+                    descriptionError.put("address", "Введите адрес клиента");
+                    break;
+                }
+                case "phone":{
+                    descriptionError.put("phone", "Введите номер телефона");
+                    break;
+                }
+            }});
+        return descriptionError;
+    }
+
+
 
     public void addNewUser(Client client) {
         clientRepository.save(client);
