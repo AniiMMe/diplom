4a3b4a8eca8d6036589090b520ad319018bfb266
Mary <parmo1977@gmail.com>
2024-05-23 21:30:22 +0300

    поправила кое что

--- src/main/java/com/example/diplom/service/OrderService.java
+++ src/main/java/com/example/diplom/service/OrderService.java
@@ -12,6 +12,7 @@ import com.example.diplom.reposiroty.ClientRepository;
 import com.example.diplom.reposiroty.OrdersRepository;
 import com.example.diplom.reposiroty.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.BindingResult;
 
@@ -22,6 +23,7 @@ import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
     private final OrdersRepository ordersRepository;
     private final ProductRepository productRepository;
@@ -30,83 +32,95 @@ public class OrderService {
 
 
     public void addNewOrder(OrderDTO order, List<OrderProductDTO> productList) {
-        List<Product> productOrder = new ArrayList<>();
-        Client client = clientRepository.getById(order.getClient());
-
-        productList.forEach(x->{
-            Assortment assortment = assortmentRepository.findById(x.getIdAssortment()).orElse(null);
-            assortment.setCount(assortment.getCount()-x.getCountProductFromAssortment());
-            List<Product> productListDB = productRepository.findAllByAssortmentOrderByProductEnddataDesc(assortment);
+        List<Product> productsListItog = new ArrayList<>();
+        productList.forEach(x -> {
+            Assortment assortment = assortmentRepository.getById(x.getIdAssortment());
+            assortment.setCount(assortment.getCount() - x.getCountProductFromAssortment());
             int count = x.getCountProductFromAssortment();
-            for (Product p: productListDB) {
-                if (p.getProductQuantity() > count) {
-                    p.setProductQuantity(p.getProductQuantity() - count);
+            List<Product> writeOffProductToOrder =
+                    productRepository.findAllByAssortmentOrderByProductEnddataDesc(assortment);
+            for (Product y : writeOffProductToOrder) {
+                if (count < y.getProductQuantity()) {
+                    y.setProductQuantity(count - y.getProductQuantity());
+                    productsListItog.add(productRepository.save(y));
                     break;
-                } else if (p.getProductQuantity() == count) {
-                    p.setProductQuantity(0);
+                } else if (count == y.getProductQuantity()) {
+                    y.setProductQuantity(0);
+                    y.setWriteOff(true);
+                    productsListItog.add(productRepository.save(y));
                     break;
                 } else {
-                    int del = count - p.getProductQuantity();
-                    p.setProductQuantity(p.getProductQuantity() - del);
-                    count = count - del;
+                    y.setProductQuantity(0);
+                    y.setWriteOff(true);
+                    count -= y.getProductQuantity();
+                    productsListItog.add(productRepository.save(y));
                 }
-                productOrder.add(productRepository.save(p));
             }
             assortmentRepository.save(assortment);
-
         });
         Orders orders = Orders.builder()
-                .orderCost(productList.stream().mapToDouble(x -> x.getCostForOneProduct() * x.getCountProductFromAssortment()).sum())
-                .orderQuantity(productList.stream().mapToDouble(OrderProductDTO::getCountProductFromAssortment).sum())
-                .orderDate(order.getOrderDate())
+                .orderCost(productList.stream().mapToDouble(x ->
+                                (x.getCostForOneProduct() * x.getCountProductFromAssortment()))
+                        .sum())
+                .orderQuantity(productList.stream().mapToInt(OrderProductDTO::getCountProductFromAssortment)
+                        .sum())
                 .orderStatus(order.getOrderStatus())
-                .client(client)
+                .orderDate(order.getOrderDate())
+                .products(productsListItog)
+                .client(clientRepository.getById(order.getClient()))
                 .build();
-        orders.setProducts(productOrder);
         ordersRepository.save(orders);
+        log.info("СФОРМИРОВАН ЗАКАЗ {}", orders.toString());
     }
-    public Map<String, String> checkErrorSwitch(OrderDTO orderDTO, List<OrderProductDTO> orderProductDTO, BindingResult result){
-        Map<String,String> descriptionError = new HashMap<>();
-        result.getFieldErrors().forEach(error ->{
-            switch (error.getField()){
-                case "orderDate":{
+
+    public Map<String, String> checkErrorSwitch(OrderDTO orderDTO, List<OrderProductDTO> orderProductDTO, BindingResult result) {
+        Map<String, String> descriptionError = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            switch (error.getField()) {
+                case "orderDate": {
                     descriptionError.put("orderDate", "Введите дату");
                     break;
                 }
-                case "idAssortment":{
+                case "idAssortment": {
                     descriptionError.put("idAssortment", "Выберите товар");
                     break;
                 }
-                case "countProductFromAssortment":{
+                case "countProductFromAssortment": {
                     descriptionError.put("countProductFromAssortment", "Введите количество товара");
                     break;
                 }
-                case "costForOneProduct":{
+                case "costForOneProduct": {
                     descriptionError.put("costForOneProduct", "Введите цену за единицу");
                     break;
                 }
-                case "client":{
+                case "client": {
                     descriptionError.put("client", "Выберите клиента");
                     break;
                 }
-                case "orderStatus":{
+                case "orderStatus": {
                     descriptionError.put("orderStatus", "Выберите статус заказа");
                     break;
                 }
-            }});
+            }
+        });
 
         return descriptionError;
     }
-    public Map<String, String> checkError(OrderProductDTO orderProductDTO, BindingResult result){
-        Map<String,String> descriptionError = new HashMap<>();
 
-        if(orderProductDTO.getIdAssortment()==0) descriptionError.put("idAssortment", "Выберите товар!");
-        if(orderProductDTO.getCountProductFromAssortment()==0) descriptionError.put("countProductFromAssortment", "Введите количество товара!");
-        if(orderProductDTO.getCostForOneProduct()==0) descriptionError.put("costForOneProduct", "Введите цену за единицу!");
-        if (orderProductDTO.getCountProductFromAssortment()<0) descriptionError.put("countProductFromAssortment","Неверное количество!");
-        if (orderProductDTO.getCostForOneProduct()<0) descriptionError.put("costForOneProduct","Неверная цена!");
+    public Map<String, String> checkError(OrderProductDTO orderProductDTO, BindingResult result) {
+        Map<String, String> descriptionError = new HashMap<>();
+
+        if (orderProductDTO.getIdAssortment() == 0) descriptionError.put("idAssortment", "Выберите товар!");
+        if (orderProductDTO.getCountProductFromAssortment() == 0)
+            descriptionError.put("countProductFromAssortment", "Введите количество товара!");
+        if (orderProductDTO.getCostForOneProduct() == 0)
+            descriptionError.put("costForOneProduct", "Введите цену за единицу!");
+        if (orderProductDTO.getCountProductFromAssortment() < 0)
+            descriptionError.put("countProductFromAssortment", "Неверное количество!");
+        if (orderProductDTO.getCostForOneProduct() < 0) descriptionError.put("costForOneProduct", "Неверная цена!");
         return descriptionError;
     }
+
     public List<Orders> getAllOrder() {
         return ordersRepository.findAll();
     }
